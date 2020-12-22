62be396a3258cd204a381eae3671c617f7fb9678
Son NK <>
2020-12-22 10:14:07 +0100

    Only Enable plausible on prod. DNT for plausible. Rename goatcounter-ignore -> analytics-ignore.

--- static/custom.js
+++ static/custom.js
@@ -1,3 +1,50 @@
+// Analytics
+
+(function () {
+  // only enable on prod
+  let host = window.location.host;
+  const sl_hosts = ["simplelogin.io", "www.simplelogin.io", "simplelogin.fr",
+    "www.simplelogin.fr", "simplelogin.co", "www.simplelogin.co"];
+
+  if (!sl_hosts.includes(host)) {
+    console.log("Analytics is only be enabled in prod");
+    return;
+  }
+
+  if (store.get('analytics-ignore') === 't') {
+    console.log("Analytics is disabled");
+    return;
+  }
+
+  console.log("init Analytics");
+
+  // GoatCounter
+  var script = document.createElement('script');
+  window.counter = 'https://sl.goatcounter.com/count'
+  script.async = 1;
+  script.src = '/lib/count.js';
+
+  var ins = document.getElementsByTagName('script')[0];
+  ins.parentNode.insertBefore(script, ins)
+
+  // Plausible
+  // <script async defer data-domain="simplelogin.io" src="https://plausible.simplelogin.io/js/index.js"></script>
+  var plausibleScript = document.createElement('script');
+  plausibleScript.defer = 1;
+  plausibleScript.async = 1;
+  plausibleScript.dataset.domain = "simplelogin.io";
+  plausibleScript.src = 'https://plausible.simplelogin.io/js/index.js';
+
+  var ins = document.getElementsByTagName('script')[0];
+  ins.parentNode.insertBefore(plausibleScript, ins)
+
+
+})();
+
+// END Analytics
+
+
+// FAQ apps
 var faqApp = new Vue({
   el: "#faq-app",
   delimiters: ["[", "]"],
